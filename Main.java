bfc8d4e0d094e5dd91f62534e0c3660470e56aac
fiscn <fiscn2709@wrdsb.ca>
2020-10-06 16:01:02 +0000

    Finished Program A5 Q5

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
- *
- * @author 
+ * program that will ask the user for a number between 1 and 10. It will then draw a line of * that is the size that was asked for.
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //prompt user to enter in a number between 1 and 10 
+    System.out.println("Please enter a number between 1 and 10"); 
+    int userNum = input.nextInt(); 
+
+    //output amount of stars that equal the value of the user's number 
+    for(int count = 1; count <= userNum; count++){
+      System.out.print("*"); 
+    }
     
   }
 }
